f6ea5f6a207829fbcf306f3f9663f909cf2da85e
Антон Медведев <iamtoxa00@gmail.com>
2020-08-06 17:53:40 +0300

    secret

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const fs = require('fs');
 const express = require('express')
+var path = require('path');
 const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
@@ -15,8 +16,19 @@ const httpsOptions = {
 
 app.prepare().then(() => {
   const expressApp = express()
-
   expressApp.all('*', (req, res) => {
+
+    console.log(req.get('host'));
+    if(req.get('host') == 'thisnotspam.ru'){
+      console.log(req.originalUrl);
+      if(req.originalUrl == "/NotEvenClose.png"){
+        res.sendFile(path.join(path.join(__dirname, 'secretpage'), 'NotEvenClose.png'));
+      } else {
+        res.sendFile(path.join(path.join(__dirname, 'secretpage'), 'index.html'));
+      }
+      return true;
+    }
+
     return handle(req, res)
   })
 
